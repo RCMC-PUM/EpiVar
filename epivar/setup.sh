516bf44565a360c98cceaa3404255c2a75da663f
JBinkowski <jan.binkowski96@gmail.com>
2025-10-17 14:23:04 +0200

    Update

--- epivar/setup.sh
+++ epivar/setup.sh
@@ -9,5 +9,5 @@ poetry run python manage.py migrate
 echo "----------- Add superuser --------- "
 poetry run python manage.py createsuperuser --no-input
 
-echo "----------- Add superuser --------- "
+echo "----------- Run app --------- "
 poetry run python -m gunicorn 'epivar.wsgi' --limit-request-field_size 8190 --limit-request-line 8190 --bind=0.0.0.0:8000 --log-level debug  --access-logfile /app/epivar/gunicorn.log  --error-logfile /app/epivar/gunicorn_error.log --timeout 120 --workers=1
